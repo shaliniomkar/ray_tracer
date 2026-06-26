8b09c98d5ab517013548ab87142b3d1d5bb35bd7
Shalini Omkar <shaliniomkar@gmail.com>
2026-06-26 21:03:26 +0530

    finished basic ray tracer

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     public:
@@ -9,6 +10,14 @@ class camera {
         int image_width = 100;      // rendered image width in pixel count
         int samples_per_pixel = 10; // count of random samples for each pixel
         int max_depth = 10;         // maximum number of ray bounces into scene
+
+        double vfov = 90; // vertical view angle (field of view)
+        point3 lookfrom = point3(0,0,0);
+        point3 lookat = point3(0,0,-1);
+        vec3 vup = vec3(0,1,0);
+
+        double defocus_angle = 0; // variation angles of rays through each pixel
+        double focus_dist = 10; // distance from camera lookfrom point to plane of perfect focus
 
         void render(const hittable& world) {
             initialize();
@@ -37,6 +46,9 @@ class camera {
         point3 pixel00_loc;         // location of pixel 0, 0
         vec3 pixel_delta_u;         // offset to pixel to the right
         vec3 pixel_delta_v;         // offset to pixel below
+        vec3 u, v, w;
+        vec3 defocus_disk_u;        // defocus disk horizontal radius
+        vec3 defocus_disk_v;        // defocus disk vertical radius
 
         void initialize() {
             image_height = int(image_width / aspect_ratio);
@@ -44,37 +56,48 @@ class camera {
 
             pixel_samples_scale = 1.0 / samples_per_pixel;
 
-            center = point3(0, 0, 0);
+            center = lookfrom;
 
             // Determine viewport dimensions.
-            auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto theta = degrees_to_radians(vfov);
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2 * h * focus_dist;
             auto viewport_width = viewport_height * (double(image_width)/image_height);
 
+            // calculate the u,v,w unit basis vectors for the camera coordinate frame
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+
             // Calculate the vectors across the horizontal and down the vertical viewport edges.
-            auto viewport_u = vec3(viewport_width, 0, 0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            vec3 viewport_u = viewport_width * u; // vector across viewport horizontal edge
+            vec3 viewport_v = viewport_height * -v; // vector down viewport vertical edge
 
             // Calculate the horizontal and vertical delta vectors from pixel to pixel.
             pixel_delta_u = viewport_u / image_width;
             pixel_delta_v = viewport_v / image_height;
 
             // Calculate the location of the upper left pixel.
-            auto viewport_upper_left =
-                center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+            // calculate the camera defocus fisk basis vectors
+            auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+            defocus_disk_u = u * defocus_radius;
+            defocus_disk_v = v * defocus_radius;
         }
 
         ray get_ray(int i, int j) const {
-            // construct a camera ray originating from the origin and
-            // directed at a randomly sampled point around the pixel location i, j.
-
+            // construct a camera ray originating from the defocus disk and directed at
+            // sampled point around the pixel location i, j.
+            
             auto offset = sample_square();
+
             auto pixel_sample = pixel00_loc
                               + ((i + offset.x()) * pixel_delta_u)
                               + ((j + offset.y()) * pixel_delta_v);
 
-            auto ray_origin = center;
+            auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
             auto ray_direction = pixel_sample - ray_origin;
 
             return ray(ray_origin, ray_direction);
@@ -85,6 +108,12 @@ class camera {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0); 
         }
 
+        point3 defocus_disk_sample() const {
+            // returns a random point in the camera defocus disk
+            auto p = random_in_unit_disk();
+            return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+        }
+
         color ray_color(const ray& r, int depth, const hittable& world) const {
             // if we've exeeded the ray bounce limit, no more light is gathered
             if (depth <= 0)
@@ -92,9 +121,13 @@ class camera {
 
             hit_record rec;
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 direction = rec.normal + random_unit_vector();
-                return 0.7 * ray_color(ray(rec.p, direction), depth-1, world);
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered))
+                    return attenuation * ray_color(scattered, depth-1, world);
+                return color(0,0,0);
             }
+               
 
             vec3 unit_direction = unit_vector(r.direction());
             auto a = 0.5*(unit_direction.y() + 1.0);
--- sphere.h
+++ sphere.h
@@ -6,8 +6,9 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
-        
+        sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(std::fmax(0,radius)), mat(mat) {}
+      
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
@@ -32,12 +33,14 @@ class sphere : public hittable {
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
             return true;
         }
     
     private:
         point3 center;
         double radius;
+        shared_ptr<material> mat;
 };
 
 #endif
