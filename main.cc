7750e04f49360e827ce857aa6ae76fddee7e0d4e
Shalini Omkar <shaliniomkar@gmail.com>
2026-06-27 23:36:55 +0530

    lowered image quality

--- main.cc
+++ main.cc
@@ -53,7 +53,7 @@ int main() {
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 1200;
-    cam.samples_per_pixel = 500;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
